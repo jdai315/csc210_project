b5b19c0a403dfd2637bcd78814bb88deb666382d
Jerry Dai <jerrydai@dhcp-10-4-16-119.wireless.rochester.edu>
2016-10-09 23:24:55 -0400

    login/register working

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 <h1>CSC 210 Final Project</h1>
+<br>
+<h2>Currently working on Milestone #2</h2>
+<br>
 Group members:
 <br>
-Vishnu Chittari
-<br>
-Jerry Dai
-<br>
-Joseph VanderStel
+<ul>
+	<li>Vishnu Chittari</li>
+	<li>Jerry Dai</li>
+	<li>Joseph VanderStel</li>
+</ul>
+
+
--- data.py
+++ data.py
@@ -2,18 +2,26 @@ import sqlite3
 
 #add new uname/pw to database
 def addNew(username, password):
+
     #connect to the database 'userpw'
-    #create a table if it doesn't exist where usernames are in 'name' and passwords are in 'pw'
     conn = sqlite3.connect("userpw")
     c = conn.cursor()
+
+    #create a table if it doesn't exist where usernames are in 'name' and passwords are in 'pw'
     c.execute('CREATE TABLE IF NOT EXISTS users(name varchar(24) primary key, pw varchar(24), UNIQUE(name, pw))')
-    
+
+    #this is where the cursor checks if the username already exists
+    c.execute('SELECT pw from users where name= ?',(username,))
+    exist = c.fetchone()
+
     #if it isnt already in the table, insert it
-    if 1==1:
+    if exist is None:
         c.execute('INSERT OR IGNORE INTO users(name, pw) VALUES(\"' + username + '\", \"' + password + '\")')
         conn.commit()
         conn.close()
         return True
+
+    #otherwise, return false to tell app.py that registering failed
     else:
         conn.commit()
         conn.close()
@@ -21,15 +29,21 @@ def addNew(username, password):
 
 #check if user/pw matches
 def check(username, password):
-    user = None
+
+    #connect to the database 'userpw'
+    conn = sqlite3.connect("userpw")
+    c = conn.cursor()
+
+    #fetch the correct password associated with this username
+    c.execute('SELECT pw from users where name= ?',(username,))
+    exist = c.fetchone()
+
     #if user doesnt exist, test fails. if password isnt the same as the stored password, test fails. otherwise, the test passes.
-    if user==None:
+    if exist is None:
+        conn.close()
         return False
     else:
-        return password == user.get("pw").encode('ascii','ignore')
-
-def count():
-    return posts.count()
-
-def show():
-    return posts.find()
+        print exist
+        conn.close()
+        #this converts the database value into a string
+        return password == exist[0].encode("ascii")
